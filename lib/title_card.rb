7ba9dc9d7f8f45602a85fa09bdfa3569eb84ace4
Eric Collins <eric@tabfugni.cc>
2021-08-30 22:41:08 -0400

    Allow for multiple line titles using captions

--- lib/title_card.rb
+++ lib/title_card.rb
@@ -20,7 +20,7 @@ class TitleCard
     add_title
     add_attribution
     add_date
-    canvas.composite_layers(logo).display
+    canvas.composite_layers(logo).write(save_filepath)
   end
 
   private
@@ -40,17 +40,20 @@ class TitleCard
   end
 
   def formatted_date
-    date.strftime("%Y/%m/%d")
+    date.strftime("%Y-%m-%d")
   end
 
   def add_title
-    text = Magick::Draw.new
-    text.font_family = FONT_FAMILY
-    text.pointsize = 32
-    text.gravity = Magick::CenterGravity
-    text.annotate(canvas, 0, 0, 0, 30, title) do
+    title_layer = Magick::ImageList.new
+    title_layer.read("caption:#{title}") do
+      self.size = "1280x750"
+      self.font = FONT_FAMILY.split(" ").join("-")
+      self.pointsize = 52
+      self.background_color = BACKGROUND_COLOR
       self.fill = TEXT_COLOR
+      self.gravity = Magick::CenterGravity
     end
+    @_canvas = canvas.composite_layers(title_layer)
   end
 
   def add_attribution
@@ -69,5 +72,13 @@ class TitleCard
     text.gravity = Magick::SouthGravity
     text.fill = TEXT_COLOR
     text.annotate(canvas, 0, 0, 0, 40, formatted_date)
+  end
+
+  def filename
+    [title.downcase, attribution.downcase, formatted_date].join("-").gsub(" ", "-")
+  end
+
+  def save_filepath
+    File.expand_path("~/tmp/#{filename}.png")
   end
 end
